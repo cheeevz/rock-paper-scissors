bd726dba6235b3bb7c26bbb04a6057fd6511090c
cheeevz <cmartin.gd@gmail.com>
2026-01-10 17:45:19 +0100

    rock paper scissors with UI & DOM modifications

--- script.js
+++ script.js
@@ -2,15 +2,62 @@ let humanScore = 0;
 let computerScore = 0;
 
 console.log("Welcome to Rock-Paper-Scissors!");
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
 
-playGame();
+const div = document.createElement("div");
+const divAnnonce = document.createElement("div");
+
+div.style.borderColor="black";
+div.style.backgroundColor = "green";
+div.style.width = "300px";
+div.style.height = "20px";
+div.style.marginTop = "20px";
+div.textContent = "Scores : PLAYER : " +humanScore +" COMPUTER : " +computerScore;
+
+document.body.appendChild(div);
+
+divAnnonce.style.borderColor="yellow";
+divAnnonce.style.backgroundColor = "green";
+divAnnonce.style.width = "300px";
+divAnnonce.style.height = "20px";
+divAnnonce.style.marginTop = "20px";
+divAnnonce.textContent = "Game in progress";
+
+document.body.appendChild(divAnnonce);
+
+rockBtn.addEventListener("click", rockSelection);
+paperBtn.addEventListener("click", paperSelection);
+scissorsBtn.addEventListener("click", scissorsSelection);
+
+function rockSelection() {
+    choice = "rock";
+    console.log("You choose Rock");
+    let computerChoice = getComputerChoice();
+    playRound(choice, computerChoice);
+}
+function paperSelection() {
+    choice = "paper";
+    console.log("You choose Paper");
+    let computerChoice = getComputerChoice();
+    playRound(choice, computerChoice);
+}
+function scissorsSelection() {
+    choice = "scissors";
+    console.log("You choose Scissors");
+    let computerChoice = getComputerChoice();
+    playRound(choice, computerChoice);
+}
+
+//playGame();
 
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-
+/*
 function getHumanChoice (){
     
     let input = prompt("Veuillez entrer votre choix");
@@ -40,7 +87,7 @@ function getHumanChoice (){
     }
 
     return choice;
-}
+} */
 
 function playRound(humanChoice, computerChoice) {
     
@@ -79,21 +126,32 @@ function playRound(humanChoice, computerChoice) {
         computerScore ++;
         console.log("Scissors beats Paper : Computer wins the round");
     }
-    
-    console.log(" Scores : Player: " +humanScore + " Computer :" +computerScore);
-}
+    divAnnonce.textContent = "Game in progress";
+    div.textContent = "Scores : PLAYER : " +humanScore +" COMPUTER : " +computerScore;
+        if (humanScore === 5){
+            divAnnonce.textContent = "PLAYER WINS";
+            humanScore = 0;
+            computerScore = 0;
+        }
+        else if (computerScore === 5){
+            divAnnonce.textContent = "COMPUTER WINS";
+            humanScore = 0;
+            computerScore = 0;
+        }
 
+}
+/*
 function playGame(){
 
-    for (let i = 0; i<= 5; i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
+    
+    //let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
     if(humanScore < computerScore){
         console.log("Game over ! You loose the game :( ")
     }
     else {
         console.log("You win the game !")
     }
-}
+} */
